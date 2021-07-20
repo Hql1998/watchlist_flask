8aa11ce1b8f38962d90e3542d709792674c93975
Huang Qinlai <huangqinlai2016@outlook.com>
2021-07-20 21:20:23 +0800

    add a simple index file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import url_for
+from flask import url_for, render_template
 
 print(__name__)
 
@@ -8,10 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def hello():
-    return """
-    <h2> Hello World! My beautiful friend!</h2>
-    <button type="submit" formaction="newdoc">Click Me!</button>
-    """
+    rendered_text = render_template("index.html", name=name, novels=novels)
+    return rendered_text
 
 @app.route("/user/<name>")
 def new_doc(name):
@@ -30,3 +28,16 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
 
+name = 'H Ql'
+novels = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
